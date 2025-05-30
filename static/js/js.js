55ad009f0b3e41460daf121b9398bed58c0d4fde
Joshua Miller <jmm.ects@gmail.com>
2025-05-30 10:26:51 -0400

    updated

---
+++ static/js/js.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const toggleBtn = document.getElementById('toggleBtn');
+    const navbar = document.getElementById('navbar');
+
+    toggleBtn.addEventListener('click', () => {
+        navbar.classList.toggle('hidden');
+    });
+});
